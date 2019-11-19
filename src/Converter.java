b17983b4c816f4532eb1aaf6808ae752a64441d1
ahmetlekesiz <ahmetlekesiz@gmail.com>
2019-11-19 14:00:50 +0300

    hashmap for instructions

---
+++ src/Converter.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+
+public class Converter {
+
+
+    //hex to binary
+    public static String hexToBin(String s) {
+        return new BigInteger(s, 16).toString(2);
+    }
+    //binary to hex
+    //decimal to binary
+    //binary to decimal
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hey");
+
+        //Initialize the Hashmap for Instructions
+        HashMap<String, String> instructions = new HashMap<>();
+        instructions.put("ADD", "0000");
+        instructions.put("ADDI", "0001");
+        instructions.put("AND", "0010");
+        instructions.put("ANDI", "0011");
+        instructions.put("OR", "0100");
+        instructions.put("ORI", "0101");
+        instructions.put("XOR", "0110");
+        instructions.put("XORI", "0111");
+        instructions.put("JUMP", "1000");
+        instructions.put("LD", "1001");
+        instructions.put("ST", "1010");
+        instructions.put("BEQ", "1011");
+        instructions.put("BLT", "1100");
+        instructions.put("BGT", "1101");
+        instructions.put("BLE", "1110");
+        instructions.put("BGE", "1111");
+
+
+
+
     }
 }
