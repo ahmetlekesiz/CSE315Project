a6ec9b02de969a4b1afc049da3629b7a01c07c8e
ahmetlekesiz <ahmetlekesiz@gmail.com>
2019-11-19 14:41:04 +0300

    hashmaps.

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,26 @@ public class Main {
 
     public static void main(String[] args) {
 
+        HashMap<String, String> instructions = initializeInstructionMap();
+        HashMap<String, String> registers = initializeRegisterMap();
+
+        //TODO
+        //Input okunup, output a çevirilecek (outputBinary.txt e yazılacak).
+        //Outut.txt deki binary'yi satır satır hex e çevir, outputHex.txt e yaz.
+
+    }
+
+    //Hashmaplerin oluşturulması için daha kısa kod yaz.
+        /*
+        //Initialize the Register for Instructions
+        HashMap<String, String> registers = new HashMap<>();
+        for (int i = 0; i < 16; i++) {
+            String value = Integer.toBinaryString(i);
+            registers.put("R" + i, i);
+        }
+        */
+
+    public static HashMap<String, String> initializeInstructionMap(){
         //Initialize the Hashmap for Instructions
         HashMap<String, String> instructions = new HashMap<>();
         instructions.put("ADD", "0000");
@@ -23,36 +43,28 @@ public class Main {
         instructions.put("BGT", "1101");
         instructions.put("BLE", "1110");
         instructions.put("BGE", "1111");
+        return instructions;
+    }
 
-
+    public static HashMap<String, String> initializeRegisterMap(){
         //Initialize the Hashmap for Registers
         HashMap<String, String> registers = new HashMap<>();
-        instructions.put("R0", "0000");
-        instructions.put("R1", "0001");
-        instructions.put("R2", "0010");
-        instructions.put("R3", "0011");
-        instructions.put("R4", "0100");
-        instructions.put("R5", "0101");
-        instructions.put("R6", "0110");
-        instructions.put("R7", "0111");
-        instructions.put("R8", "1000");
-        instructions.put("R9", "1001");
-        instructions.put("R10", "1010");
-        instructions.put("R11", "1011");
-        instructions.put("R12", "1100");
-        instructions.put("R13", "1101");
-        instructions.put("R14", "1110");
-        instructions.put("R15", "1111");
-
-        //Hashmaplerin oluşturulması için daha kısa kod yaz.
-        /*
-        //Initialize the Register for Instructions
-        HashMap<String, String> registers = new HashMap<>();
-        for (int i = 0; i < 16; i++) {
-            String value = Integer.toBinaryString(i);
-            registers.put("R" + i, i);
-        }
-        */
-
+        registers.put("R0", "0000");
+        registers.put("R1", "0001");
+        registers.put("R2", "0010");
+        registers.put("R3", "0011");
+        registers.put("R4", "0100");
+        registers.put("R5", "0101");
+        registers.put("R6", "0110");
+        registers.put("R7", "0111");
+        registers.put("R8", "1000");
+        registers.put("R9", "1001");
+        registers.put("R10", "1010");
+        registers.put("R11", "1011");
+        registers.put("R12", "1100");
+        registers.put("R13", "1101");
+        registers.put("R14", "1110");
+        registers.put("R15", "1111");
+        return registers;
     }
 }
