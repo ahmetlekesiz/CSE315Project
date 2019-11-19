18c8715962c5aaec799b794009bf4458ff1815df
Enver ASLAN <enver.asln@gmail.com>
2019-11-19 22:33:21 +0300

    Read file is completed.

--- src/Converter.java
+++ src/Converter.java
@@ -7,8 +7,20 @@ public class Converter {
     public static String hexToBin(String s) {
         return new BigInteger(s, 16).toString(2);
     }
+
     //binary to hex
+    public static String binToHex(String s) {
+        int decimal  = Integer.parseInt(s, 2);
+        String hexStr = Integer.toString(decimal, 16);
+        return hexStr.toUpperCase();
+    }
+
     //decimal to binary
+    public static String decToBin(String d) {
+        int decimal = Integer.parseInt(d);
+        String bin = Integer.toBinaryString(decimal);
+        return bin;
+    }
     //binary to decimal
 
 }
